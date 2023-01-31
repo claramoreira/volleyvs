d5a718e99991c37a49963ff59daaa193955c9e81
Clara <clara_ljm@hotmail.com>
2023-01-31 10:06:03 -0300

    create mainview scene

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,30 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			AnchorPane anchorPane = loader.load();
+			
+			
+			mainScene = new Scene(anchorPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("VolleyVs");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
