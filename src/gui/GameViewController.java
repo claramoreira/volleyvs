dc99e805c87dcc49f0afaa19a77afe56688db0ad
Clara <clara_ljm@hotmail.com>
2023-02-08 08:30:55 -0300

    creating GameViewController + adjusting GameView.fxml

---
+++ src/gui/GameViewController.java
@@ -0,0 +1,48 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GameViewController {
+	
+	@FXML
+	private Label labelFirstTeam;
+	
+	@FXML
+	private Label labelSecondTeam;
+	
+	@FXML
+	private Label labelFirstTeamFirstSet;
+	
+	@FXML
+	private Label labelFirstTeamSecondSet;
+	
+	@FXML
+	private Label labelFirstTeamThirdSet;
+	
+	@FXML
+	private Label labelFirstTeamFourthSet;
+	
+	@FXML
+	private Label labelFirstTeamFifthSet;
+	
+	@FXML
+	private Label labelSecondTeamFirstSet;
+	
+	@FXML
+	private Label labelSecondTeamSecondSet;
+	
+	@FXML
+	private Label labelSecondTeamThirdSet;
+	
+	@FXML
+	private Label labelSecondTeamFourthSet;
+	
+	@FXML
+	private Label labelSecondTeamFifthSet;
+	
+	@FXML
+	private Label labelWinningTeam;
+	
+
+}
